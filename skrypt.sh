477f184bdc9c1fc95552a79220abd4650109f991
Dabowski <ukrytytomek@gmail.pl>
2022-07-01 22:47:51 +0200

    On branch dziewiec  Changes to be committed: 	new file:   error1.txt 	new file:   error10.txt 	new file:   error2.txt 	new file:   error3.txt 	new file:   error4.txt 	new file:   error5.txt 	new file:   error6.txt 	new file:   error7.txt 	new file:   error8.txt 	new file:   error9.txt 	modified:   skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -12,7 +12,7 @@ logs ()
 
     while [ $i -gt "0" ] 
     do
-        echo "log$i.txt, $nazwa, $(date)" >> log$i.txt
+        echo "$2$i.txt, $nazwa, $(date)" >> $2$i.txt
         i=$(( $i - 1 ))
     done
 }
@@ -27,12 +27,15 @@ help ()
 {
     echo "--date | -d - wyświetla dzisiejszą datę i godzinę"
     echo "--logs | -l - tworzy 100 plików logx.txt (x to liczba od 1 do 100)"
+    echo "--error | -e - tworzy 100 plików errorx.txt (x to liczba od 1 do 100)"
     echo "--logs n | -l n- tworzy n plików logx.txt (x to liczba od 1 do n)"
+    echo "--error n | -e n- tworzy n plików errorx.txt (x to liczba od 1 do n)"
     echo "--help | -h - wyświetla opcje"
 }
 
 case "$opcja" in
     "--date" | "-d") date;;
-    "--logs" | "-l") logs $2;;
+    "--logs" | "-l") logs $2 "log";;
+    "--error" | "-e") logs $2 "error";;
     "--help" | "-h") help;;
 esac
