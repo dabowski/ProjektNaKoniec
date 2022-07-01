3f46d02d675624beb8b94c7e591188d8506faf13
Dabowski <ukrytytomek@gmail.pl>
2022-07-01 21:43:10 +0200

    On branch trzeci  Changes to be committed: 	modified:   skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,15 @@
 opcja=$1
+nazwa=$0
+
+logs ()
+{
+    i=100
+    while [ $i -gt "0" ] 
+    do
+        echo "log$i.txt, $nazwa, $(date)" >> log$i.txt
+        i=$(( $i - 1 ))
+    done
+}
 
 case "$opcja" in
     "--date") date;;
